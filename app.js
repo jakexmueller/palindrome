be07fd52e2d3a62dc39b8f73887bc4870e0f89d0
Jacob <jakexmueller@gmail.com>
2018-05-14 08:53:23 -0500

    adding files

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 "use strict"
+
+
+runPalindrome()
+function runPalindrome () {
+	let word = getUserInput("Type a word to see if it is a Palindrome.");
+	let newWord = reverseString (word);
+		
+		if (newWord === word) {
+			document.write("You have succeeded the Palindrome.");
+		}	
+		else {
+			document.write("You have failed the Palindrome.");
+		}
+	
+}
+
+
+
+
+function getUserInput (word) {
+	let response = prompt(word);
+	return response;
+	
+	
+}
+
+function reverseString (word) {
+	let splitString = word.split("");
+	let reverseArray = splitString.reverse();
+	let newWord = reverseArray.join("");
+	return newWord;
+}
